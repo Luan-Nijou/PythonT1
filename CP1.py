a4bb06a57f9673699cf6ff653de66fe4b0a2ec1b
Luna-Nijou <RM98920@fiap.com.br>
2023-04-14 22:09:27 -0300

    final

--- CP1.py
+++ CP1.py
@@ -9,15 +9,15 @@ MeioDeCompra = input("Qual o meio de compra(Exemplos: Cartão de crédito): ")
 CatalogoVinho = ("Vinhos a venda ---> (1)Vinho_A->R$50 / (2)Vinho_B->R$100 / (3)Vinho_C->R$150 / (4)Vinho_D->R$200 / (5)Vinho_E->R$250")
 
 
-QtdViA = 0
+QuantidadeVinhoA = 0
 ValorViA = 0
-QtdViB = 0
+QuantidadeVinhoB = 0
 ValorViB = 0
-QtdViC = 0
+QuantidadeVinhoC = 0
 ValorViC = 0
-QtdViD = 0
+QuantidadeVinhoD = 0
 ValorViD = 0
-QtdViE = 0
+QuantidadeVinhoE = 0
 ValorViE = 0
 ValorTotalCompra = 0
 Frete = 10
@@ -28,16 +28,16 @@ if Idade<18:
 elif Idade>=18:
     print(CatalogoVinho)
     print("Escolha a quantidade de cada vinho que irá comprar, valores acima de 200 reais recebem frete grátis(frete = 10 reais)")
-    QtdViA = int(input("Qtd VinhoA:"))
-    QtdViB = int(input("Qtd VinhoB:"))
-    QtdViC = int(input("Qtd VinhoC:"))
-    QtdViD = int(input("Qtd VinhoD:"))
-    QtdViE = int(input("Qtd VinhoE:"))
-    ValorViA = 50 * QtdViA
-    ValorViB = 100 * QtdViB
-    ValorViC = 150 * QtdViC
-    ValorViD = 200 * QtdViD
-    ValorViE = 250 * QtdViE
+    QuantidadeVinhoA = int(input("Quantidade do VinhoA:"))
+    QuantidadeVinhoB = int(input("Quantidade do VinhoB:"))
+    QuantidadeVinhoC = int(input("Quantidade do VinhoC:"))
+    QuantidadeVinhoD = int(input("Quantidade do VinhoD:"))
+    QuantidadeVinhoE = int(input("Quantidade do VinhoE:"))
+    ValorViA = 50 * QuantidadeVinhoA
+    ValorViB = 100 * QuantidadeVinhoB
+    ValorViC = 150 * QuantidadeVinhoC
+    ValorViD = 200 * QuantidadeVinhoD
+    ValorViE = 250 * QuantidadeVinhoE
     ValorTotalCompra = ValorViA + ValorViB + ValorViC + ValorViD + ValorViE
 
     if ValorTotalCompra<100:
